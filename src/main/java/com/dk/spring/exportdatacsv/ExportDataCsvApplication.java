76c8449937c5095ecc56ff0240967e0fa8aa3538
Dávid Kecskeméti <dxdy421@gmail.com>
2020-10-11 16:26:38 +0200

    Initial commit

---
+++ src/main/java/com/dk/spring/exportdatacsv/ExportDataCsvApplication.java
@@ -0,0 +1,13 @@
+package com.dk.spring.exportdatacsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExportDataCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExportDataCsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dk/spring/exportdatacsv/ExportDataCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dk.spring.exportdatacsv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExportDataCsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
