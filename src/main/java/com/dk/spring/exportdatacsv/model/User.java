fc80b6403d5c6f92dc9d4acc69390f7c1377e0fb
Dávid Kecskeméti <dxdy421@gmail.com>
2020-10-15 10:14:53 +0200

    updated

--- src/main/java/com/dk/spring/exportdatacsv/model/User.java
+++ src/main/java/com/dk/spring/exportdatacsv/model/User.java
@@ -1,5 +1,14 @@
 package com.dk.spring.exportdatacsv.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
 public class User {
 
 //    Enable columns positions if you want to
@@ -16,62 +25,4 @@ public class User {
 //    @CsvBindByPosition(position = 4)
     private int age;
 
-    public User(long id, String name, String email, String country, int age) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.country = country;
-        this.age = age;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", country='" + country + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
